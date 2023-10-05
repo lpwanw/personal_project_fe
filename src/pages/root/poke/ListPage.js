8b3419e1f11fd540d0870f78069dd46de4710a57
lpwanw <phuongtay2000@gmail.com>
2023-10-05 11:05:27 +0700

    Paging

--- src/pages/root/poke/ListPage.js
+++ src/pages/root/poke/ListPage.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
+import ReactPaginate from "react-paginate";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from "react-responsive-carousel";
 import Type from "../../../components/pokemon/Type";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBackward, faForward} from "@fortawesome/free-solid-svg-icons";
 
 function PokemonItem({pokeName}) {
   const [pokemon, setPokemon] = useState(null);
@@ -18,7 +21,7 @@ function PokemonItem({pokeName}) {
   if (!pokemon) return <div>Loading...</div>;
 
   return(
-    <div className="flex border border-slate-500 hover:border-slate-200 rounded-xl p-4">
+    <div className="flex border border-slate-500 hover:border-slate-200 rounded-xl p-2">
       <div className="w-1/2">
         <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true} showIndicators={false} showArrows={false} showStatus={false}>
           {Object.entries(pokemon.sprites).filter(([key, value]) => { return value}).map(([key, value]) => {
@@ -43,24 +46,47 @@ function PokemonItem({pokeName}) {
 
 export default function ListPage() {
   const [pokemons, setPokemons] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(null);
 
   useEffect(() => {
-    // Fetch individual Pokémon data
-    fetch(`https://pokeapi.co/api/v2/pokemon`)
+    const limit = 20;
+    const offset = page * limit;
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
       .then(response => response.json())
-      .then(data => setPokemons(data))
+      .then(data => {
+        setPokemons(data.results);
+        setTotalPage(Math.round(data.count / limit))
+      })
       .catch(err => console.error(err));
-  }, []);
+  }, [page]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    setPage(event.selected)
+  };
 
   if (!pokemons) return <div>Loading...</div>;
 
   return(<>
     <div className="grid grid-cols-4 gap-4">
       {
-        pokemons.results.map(pokemon => {
+        pokemons.map(pokemon => {
           return <PokemonItem key={pokemon.name} pokeName={pokemon.name}/>
         })
       }
     </div>
+    <ReactPaginate
+        breakLabel="..."
+        nextLabel={page === (totalPage - 1) ? null : <FontAwesomeIcon icon={faForward} />}
+        onPageChange={handlePageClick}
+        containerClassName="flex justify-center border mt-4 p-2 w-full"
+        pageClassName="border rounded mx-2 px-2"
+        activeClassName={`bg-slate-500`}
+        pageRangeDisplayed={5}
+        pageCount={totalPage}
+        previousLabel={page === 0 ? null : <FontAwesomeIcon icon={faBackward} />}
+        renderOnZeroPageCount={null}
+    />
   </>)
 }
